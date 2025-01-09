394e5265a24fc19a542d0906369a35779a650537
dilshod <dilshodkarimov242@gmail.com>
2025-01-09 15:39:11 +0500

    7

--- src/main/java/uz/app/finalproject/FinalProjectApplication.java
+++ src/main/java/uz/app/finalproject/FinalProjectApplication.java
@@ -2,8 +2,10 @@ package uz.app.finalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class FinalProjectApplication {
 
     public static void main(String[] args) {
